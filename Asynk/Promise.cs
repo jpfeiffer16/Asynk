0393a2f443f0b1797ddccd33db6ef7bc32482ca4
jpfeiffer16 <jpfeiffer@bluemodus.com>
2016-01-22 12:41:29 -0700

    Initial commit

---
+++ Asynk/Promise.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Asynk
+{
+    public class Promise
+    {
+        public bool Resolved { get; set; }
+        public object Value { get; set; }
+
+        private Action Callback { get; set; }
+        private Action OnResolved { get; set; }
+        private Promise PreviousPromise { get; set; }
+
+        public Promise(Promise previousPromise)
+        {
+            PreviousPromise = previousPromise;
+            Task.Run(() => 
+            {
+                while (PreviousPromise.Resolved == false)
+                {
+                    Thread.Sleep(10);
+                }
+                if (OnResolved != null)
+                {
+                    Resolve();
+                }
+            });
+        }
+
+        public Promise(Action callback)
+        {
+            Callback = callback;
+            Task.Run(() =>
+            {
+                Callback();
+                Resolve();
+            });
+        }
+
+        public void Resolve()
+        {
+            Value = null;
+            Resolved = true;
+            if (OnResolved != null)
+            {
+                OnResolved();
+            }
+        }
+
+        public Promise Then(Action callback) 
+        {
+            OnResolved = callback;
+            return new Promise(this);
+        }
+
+
+        //This is not going to work right now. Have to update the Promise class to use a .Resolve() pattern istead of new Promise(previousPromise) pattern
+        //public Promise When(Promise otherPromise, Action callback)
+        //{
+        //    var promise = new Promise(this);
+        //    Task.Run(() =>
+        //    {
+        //        while (PreviousPromise.Resolved == false)
+        //        {
+        //            Thread.Sleep(100);
+        //        }
+        //    });
+        //    return promise;
+        //}
+    }
+}
---
+++ app/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Asynk;
+
+
+namespace app
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var utils = new Utils();
+            utils.DoStuff()
+                .Then(() =>
+                {
+                    Console.WriteLine("Done doing stuff");
+                });
+            
+            //Keep the main ui thread awake:
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    public class Utils
+    {
+        public Promise DoStuff()
+        {
+            return new Promise(() =>
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(i.ToString());
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
